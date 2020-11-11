577c112ce4e2f8ac66231803cc56c0be25edbfe4
William Y Wong <wwong1987@gmail.com>
2020-11-11 15:42:12 -0800

    set up react hooks to get simple data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import config from './config';
 
 function App() {
-  return (
-    <main className='App'>
-      <h1>U-Pick Project</h1>
-    </main>
-  );
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(`${config.API_ENDPOINT}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  // console.log(items[0])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <h1>You Pick API Client</h1>
+        <p>{items[0]}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    API_ENDPOINT: 'http://localhost:8000',
+    // API_ENDPOINT: 'https://pacific-waters-53419.herokuapp.com/',
+    TOKEN_KEY: 'super-secret-secret'
+}
