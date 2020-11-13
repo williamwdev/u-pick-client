4024fb73b675198e62921f220807e324ea3315ae
William Y Wong <wwong1987@gmail.com>
2020-11-12 20:38:08 -0800

    initialized smoke and snapshot test and passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  // smoke test
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+// snapshot test
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
